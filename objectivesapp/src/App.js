240007fa2a5073da5d5480521c173a47f5693dea
ryanmutula <ryan.mutula@student.moringaschool.com>
2024-05-13 17:06:52 +0300

    Halfway through styling

--- objectivesapp/src/App.js
+++ objectivesapp/src/App.js
@@ -13,7 +13,7 @@ function App() {
             </div>
             <div className='objectives-input-item'>
               <label>Description</label>
-              <input type='text' placeholder="Write down it's description here"/>
+              <input type='text' placeholder="Write down description here"/>
             </div>
             <div className='objectives-input-item'>
               <button type='button' className='primary-button'>Add</button>
@@ -29,7 +29,7 @@ function App() {
                 <h3>Objective 1</h3>
                 <p>Description</p>
               </div>
-              
+
           </div>
         </div>
     </div>
