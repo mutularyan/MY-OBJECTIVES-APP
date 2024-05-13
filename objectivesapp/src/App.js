34028806e560148416967b34a6dcaeb094c6e0bd
ryanmutula <ryan.mutula@student.moringaschool.com>
2024-05-13 16:38:22 +0300

    Done with the basic structure of my app

--- objectivesapp/src/App.js
+++ objectivesapp/src/App.js
@@ -4,6 +4,34 @@ function App() {
   return (
     <div className="App">
       <h1>My Objectives</h1>
+
+       <div className='objectives-wrapper'>
+          <div className='objectives-input'>
+            <div className='objectives-input-item'>
+              <label>Objective</label>
+              <input type='text' placeholder='Write down objective here'/>
+            </div>
+            <div className='objectives-input-item'>
+              <label>Description</label>
+              <input type='text' placeholder="Write down it's description here"/>
+            </div>
+            <div className='objectives-input-item'>
+              <button type='button' className='primary-button'>Add</button>
+            </div>
+          </div> 
+
+          <div className='btn-area'>
+             <button>Objective</button>
+             <button>Completed</button>
+          </div>
+          <div className='objectives-list'>
+              <div className='objectives-list-item'>
+                <h3>Objective 1</h3>
+                <p>Description</p>
+              </div>
+              
+          </div>
+        </div>
     </div>
   )
 }
